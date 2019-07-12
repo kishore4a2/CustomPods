6803b7fef0f85f44a44032dc3d8af797c3708b85
Kishore <kishore@11street.my>
2019-07-12 18:04:00 +0800

    Initial commit

---
+++ Nikhila/Nikhila.h
@@ -0,0 +1,19 @@
+//
+//  Nikhila.h
+//  Nikhila
+//
+//  Created by admin on 12/07/2019.
+//  Copyright © 2019 11Street. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Nikhila.
+FOUNDATION_EXPORT double NikhilaVersionNumber;
+
+//! Project version string for Nikhila.
+FOUNDATION_EXPORT const unsigned char NikhilaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Nikhila/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Nikhila' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Nikhila
+  pod 'Alamofire', '~> 4.7'
+  pod 'AlamofireObjectMapper', '~> 5.2'
+  pod 'RxSwift', '~> 5'
+  pod 'RxCocoa', '~> 5'
+  pod 'Firebase/Core'
+  pod 'Firebase/RemoteConfig'
+  pod 'FirebaseFirestore'
+  pod 'Sync', '~> 5'
+  pod 'Toast-Swift', '~> 5.0.0'
+end
