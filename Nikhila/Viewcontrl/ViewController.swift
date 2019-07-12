bed6098a68126eb9a0a13a4f4d548ca2051bab49
Kishore <kishore@11street.my>
2019-07-12 19:49:42 +0800

    Pods

---
+++ Nikhila/Viewcontrl/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Sravan
+//
+//  Created by admin on 24/05/2018.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func click(_ sender: Any) {
+        print("printing from cocoa pod")
+    }
+    
+}
+
